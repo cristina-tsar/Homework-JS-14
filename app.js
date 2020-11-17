1c3284b6f10d53732cfc97adc137ab25b9a3ca06
cristina-tsar <73910909+cristina-tsar@users.noreply.github.com>
2020-11-17 18:21:36 +0200

    changed text -home-15

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let age = prompt ('Please, enter your age!','');
+/*let age = prompt ('Please, enter your age!','');
 if (age < 18 || age > 45) {
     console.log ('You are good for us!');
 } else {
@@ -39,7 +39,70 @@ if ((a > 3 && a < 12) && ( b >= 5 && b < 13)) {
  }
  let userPercentage =  ((userDeposit*(userInterest/100)/12)*userMonth);//зароблені гроші
  
- alert('Шановний '+ userName +', Ви внесли '+ userDeposit +' грн., під '+userInterest+'% річних, на термін '+ userMonth +' місяців. За цей період часу, Ви, заробите '+ userPercentage +' грн.');
+ alert('Шановний '+ userName +', Ви внесли '
+ + userDeposit +' грн., під '+userInterest
+ +'% річних, на термін '+ userMonth 
+ +' місяців. За цей період часу, Ви, заробите '
+ + userPercentage +' грн.');*/
+//Задача 1
+
+let sale = 20;
+let price = +prompt('Яка ціна товару в грн.?','');
+
+function calcSum() {
+   return price - price*(sale/100)
+}
+
+console.log (calcSum() + ' грн.')
+
+//Задача 2
+
+function max(a,b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+console.log (max( 2,10));
+console.log (max( 8,1));
+console.log (max( 22,21));
+
+
+//Задача 3
+
+let a = +prompt ('Введіть ціну товару!','');
+let b = +prompt ('Введіть кількість товарів!','');
+
+function calcSum (a,b) {
+
+    console.log (a * b)
+}
+calcSum(a,b)
+
+//Задача 4
+
+let a = +prompt('Введіть загальну суму покупок!','');
+let sale;
+function calcSum(a,sale) {
+    
+        if ( a < 1000) {
+        console.log ( a = a - a * 0.03);
+        sale = 3;
+    } else if ( a >= 1000 && a < 5000) {
+
+        console.log ( a = a - a * 0.05);
+        sale = 5;    
+
+    } else if ( a >= 5000) {
+        console.log (a = a - a * 0.1);
+        sale = 10;
+    }
+    console.log ('Ваша знижка -'+ sale + '%.'
+    + ' Сума оплати становить - ' +  a );
+}
+
+calcSum(a, sale);
     
 
 
