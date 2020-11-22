02ef56914c5bad8b17f5076e4a1a78cca6564f5b
cristina-tsar <73910909+cristina-tsar@users.noreply.github.com>
2020-11-22 22:33:45 +0200

    changed code

--- app.js
+++ app.js
@@ -1,109 +1,79 @@
-/*let age = prompt ('Please, enter your age!','');
-if (age < 18 || age > 45) {
-    console.log ('You are good for us!');
-} else {
-    console.log ('Sorry, you are not the rigth man.');
-}
+//Завдання 1
 
-
-let minute = prompt ('Enter number from 0 to 59.','');
-if (minute >= 0 && minute <= 14) {
-    console.log ('the first quarter of an hour.');
-} else if (minute >= 15 && minute <= 29) {
-    console.log ('the second quarter of an hour.');
-} else if (minute >= 30 && minute <= 44){
-    console.log ('the third quarter of an hour.');
-} else if (minute >= 45 && minute <= 59) {
-    console.log ('the last quarter of an hour.');
-}
-
-let a = prompt('Entre number a.','');
-let b = prompt ('Enter number b.','');
-
-if ((a > 3 && a < 12) && ( b >= 5 && b < 13)) {
-    console.log ('true') 
-}   else {
-    console.log ("false");
-}
-
- let userName = prompt ("Доброго дня! Введіть, будь ласка, Ваше ім'я!", "");//ім'я клієнта
- let userDeposit = prompt("Яку суму, Ви, бажаєте внести?", "");// сума депозиту
- let userMonth = prompt("На який термір, Ви, бажаєте внести цю суму? (Введіть кількість місяців)", "");//строк дії депозиту
- let userInterest = '';
- if (userMonth < 6) {
-     userInterest = 15;
- } else if ( userMonth >= 6 && userMonth < 9) {
-     userInterest = 16;
- } else if ( userMonth >= 9) {
-     userInterest = 17;
- }
- let userPercentage =  ((userDeposit*(userInterest/100)/12)*userMonth);//зароблені гроші
- 
- alert('Шановний '+ userName +', Ви внесли '
- + userDeposit +' грн., під '+userInterest
- +'% річних, на термін '+ userMonth 
- +' місяців. За цей період часу, Ви, заробите '
- + userPercentage +' грн.');*/
-
-//Задача 1
-
-let ride = 20;
-let price = +prompt('Яка ціна товару в грн.?','');
-
-function calcSum(ride,price) {
-   return price - price*(ride/100)
-}
-
-console.log (calcSum(ride,price) + ' грн.');
-
-//Задача 2
-
-function max(x,y) {
-    if (x > y) {
-        return x;
+function ask(num1,num2,answer) {
+    if (confirm(answer)) {
+        return num1()
     } else {
-        return y;
+        return num2()
     }
 }
-console.log (max( 2,10));
-console.log (max( 8,1));
-console.log (max( 22,21));
 
-
-//Задача 3
-
-let cost = +prompt ('Введіть ціну товару!','');
-let number = +prompt ('Введіть кількість товарів!','');
-
-function calcNum (cost,number) {
-
-    console.log (cost * number)
+function showNum1() {
+    console.log (8)
 }
-calcNum(cost,number);
 
-//Задача 4
+function showNum2() {
+    console.log (6)
+}
 
-let a = +prompt('Введіть загальну суму покупок!','');
-let sale;
-function checkPermission(a,sale) {
-    
-        if ( a < 1000) {
-        console.log ( a = a - a * 0.03);
-        sale = 3;
-    } else if ( a >= 1000 && a < 5000) {
+ask(showNum1, showNum2,"Are you confirm?")
 
-        console.log ( a = a - a * 0.05);
-        sale = 5;    
+//Задання 2.1
+let askYear = +prompt('When were you born?','');
+let yearNow = 2020;
+function calcAge() {
+    yearOld = yearNow - askYear;
+    return yearOld;
+}
+console.log(calcAge())
 
-    } else if ( a >= 5000) {
-        console.log (a = a - a * 0.1);
-        sale = 10;
+//2.2
+let userName = prompt('Як, Вас, звати?','');
+let year = 60 - yearOld;
+function yearUntilRetirement(year,userName) {
+    if (yearOld < 60) {
+        console.log (`${userName} вийде на пенсію через ${year} років`)
+
+    } else if(yearOld >= 60){
+        console.log (`${userName} вже пенсіонер.`) 
     }
-    console.log ('Ваша знижка -'+ sale + '%.'
-    + ' Сума оплати становить - ' +  a );
-}
 
-checkPermission(a, sale);
+}
+console.log(yearUntilRetirement())
+
+//Заввдання 3
+
+let n = +prompt('Введіть порядковий номер дня.','');
+
+function weekFn(n) {
+    switch (n){
+        case 1:
+            console.log('Понеділок');
+            break;
+        case 2:
+            console.log('Вівторок');
+            break;
+        case 3:
+            console.log('Середа');
+            break;
+        case 4:
+            console.log('Четвер');
+            break;
+        case 5:
+            console.log('П"ятниця');
+            break;
+        case 6:
+            console.log('Субота');
+            break;
+        case 7:
+            console.log('Неділя');
+            break;
+        default:
+            console.log(null);
+    }
+}
+console.log(weekFn(n))
+
 
 
 
