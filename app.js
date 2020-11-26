16c31c0bc55f5af40ae828cce4c912801da475d9
cristina-tsar <73910909+cristina-tsar@users.noreply.github.com>
2020-11-26 18:04:54 +0200

    fixed homework

--- app.js
+++ app.js
@@ -1,79 +1,52 @@
 //Завдання 1
 
-function ask(num1,num2,answer) {
+let num1 = +prompt("","");
+let num2 = +prompt ("","");
+
+function foo(num1,num2,answer) {
     if (confirm(answer)) {
-        return num1()
-    } else {
-        return num2()
+        if (num1 > num2){
+        return num1;
+    } else if ( num1 < num2) {
+        return num2;
     }
+    } else {
+        if (num1 < num2){
+            return num1;
+        } else {
+            return num2;
+        }
+    }  
 }
+console.log(foo(num1, num2,"Are you confirm?"));
 
-function showNum1() {
-    console.log (8)
-}
-
-function showNum2() {
-    console.log (6)
-}
-
-ask(showNum1, showNum2,"Are you confirm?")
 
 //Задання 2.1
 let askYear = +prompt('When were you born?','');
 let yearNow = 2020;
-function calcAge() {
+function calcAge(yearOld) {
     yearOld = yearNow - askYear;
     return yearOld;
 }
-console.log(calcAge())
+console.log(calcAge());
 
 //2.2
 let userName = prompt('Як, Вас, звати?','');
-let year = 60 - yearOld;
-function yearUntilRetirement(year,userName) {
-    if (yearOld < 60) {
-        console.log (`${userName} вийде на пенсію через ${year} років`)
+let year = calcAge();
 
-    } else if(yearOld >= 60){
+function yearUntilRetirement(year,userName) {
+    
+    if (year < 60) {
+        console.log (`${userName} вийде на пенсію через ${60 - year} років`)
+    } else {
         console.log (`${userName} вже пенсіонер.`) 
     }
-
 }
-console.log(yearUntilRetirement())
 
-//Заввдання 3
+yearUntilRetirement(year,userName);
+  
 
-let n = +prompt('Введіть порядковий номер дня.','');
-
-function weekFn(n) {
-    switch (n){
-        case 1:
-            console.log('Понеділок');
-            break;
-        case 2:
-            console.log('Вівторок');
-            break;
-        case 3:
-            console.log('Середа');
-            break;
-        case 4:
-            console.log('Четвер');
-            break;
-        case 5:
-            console.log('П"ятниця');
-            break;
-        case 6:
-            console.log('Субота');
-            break;
-        case 7:
-            console.log('Неділя');
-            break;
-        default:
-            console.log(null);
-    }
-}
-console.log(weekFn(n))
-
+          
 
 
 
